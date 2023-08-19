3248de9f6644ebc896c529d6b1edc3e0d2a67829
alirezanobakht2004 <alireza.nobakht2004@gmail.com>
2023-08-19 13:53:37 +0330

    v1.0

---
+++ js/app.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+
+    $(window).on('load',function(){
+        $('.preloader').addClass('complete');
+    })
+
+    $(window).on('scroll', function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 50) {
+            $(".sticky").addClass("stickyadd");
+        } else {
+            $(".sticky").removeClass("stickyadd");
+        }
+    })
+
+    var typed = new Typed(".element", {
+        strings: ["lireza Nobakht", "&nbsp;developer", "&nbsp;trader", "&nbsp;fisherman"],
+        smartBackspace: true,
+        typeSpeed: 100,
+        backSpeed: 100,
+        loop: true,
+        loopCount: Infinity,
+        startDelay: 1000
+    });
+
+    var waypoint = new Waypoint({
+        element: document.getElementById('exp-id'),
+        handler: function () {
+            var p = document.querySelectorAll('.progress-bar');
+            p[0].setAttribute("style", "width:85%;transition:1s all");
+            p[1].setAttribute("style", "width:90%;transition:1.5s all");
+            p[2].setAttribute("style", "width:85%;transition:2s all");
+            p[3].setAttribute("style", "width:75%;transition:2.5s all");
+            p[4].setAttribute("style", "width:65%;transition:3s all");
+        },
+        offset: '90%'
+    });
+
+    var filterizd = $('.filter-container').filterizr({
+        animationDuration : .5,
+    });
+
+
+});
+
+
